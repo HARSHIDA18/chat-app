80a4ce407b73240d9eaf726bbf91a7a22b50df26
Harshida Shaily <harshidashaily@gmail.com>
2022-06-28 01:03:26 +0530

    started building the sidebar component

---
+++ .history/src/components/Sidebar_20220628004547.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './dashboard';
+
+const Sidebar = () => {
+  return (
+    <div className="h=100 pt-2">
+      <div>
+        <Dashboard />
+      </div>
+      bottom
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ .history/src/pages/Home_20220628003016.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return <Grid fluid className="h-100">
+    <Row>
+      <Col xs={24} md={8}>
+        <Sidebar/>
+      </Col>
+    </Row>
+  </Grid>>
+};
+
+export default Home;
