f88fc5ea3f3ed1c84e86e6146620cf9f5a0c3e76
Harshida Shaily <harshidashaily@gmail.com>
2022-06-28 03:33:10 +0530

    started to work with user avatar upload

---
+++ .history/src/components/dashboard/AvatarUploadBtn_20220628032049.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Modal, Button, ALert } from 'rsuite';
+import { useModalState } from '../../misc/custom-hooks';
+
+const fileInputTypes = '.png, .jpeg, .jpg';
+
+const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
+const isValidFile = file => acceptedFileTypes.includes(file.type);
+
+const AvatarUploadBtn = () => {
+  const { isOpen, open, close } = useModalState();
+  const onFileInputChange = ev => {
+    const currFiles = ev.target.files;
+    if (currFiles.length === 1) {
+      const file = currFiles[0];
+      if (isValidFile(file)) {
+      } else {
+        Alert.warning(`Wrong file type ${file.type}`, 4000);
+      }
+    }
+  };
+  return (
+    <div className="mt-3 text-center">
+      <div>
+        <label
+          htmlFor="avatar-upload"
+          className="d-block cursor-pointer padded"
+        >
+          Select new avatar
+          <input
+            id="avatar-upload"
+            type="file"
+            className="d-none"
+            accept={fileInputTypes}
+            onChange={onFileInputChange}
+          />
+        </label>
+        <Modal show={isOpen} onHide={close}>
+          <Modal.Header>
+            <Modal.Title>Adjust and upload new avatar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>xxx</Modal.Body>
+          <Modal.Footer>
+            <Button appearance="ghost">Upload new avatar</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default AvatarUploadBtn;
