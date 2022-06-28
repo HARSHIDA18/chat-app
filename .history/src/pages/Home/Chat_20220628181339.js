0575e0addddea0f942b500b834dc5031f2b05eff
Harshida Shaily <harshidashaily@gmail.com>
2022-06-28 19:44:59 +0530

    created layout for chat page

---
+++ .history/src/pages/Home/Chat_20220628181339.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ChatTop from '../../components/chat-window/top';
+import ChatTop from '../../components/chat-window/bottom';
+import ChatTop from '../../components/chat-window/messages';
+
+const Chat = () => {
+  return (
+    <div>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="class-bottom">
+        <ChatBottom />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- .history/src/pages/Home/Chat_20220628182424.js
+++ .history/src/pages/Home/Chat_20220628182424.js
@@ -11,14 +11,9 @@ const Chat = () => {
   const rooms = useRooms();
 
   if (!rooms) {
-    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+    return <Loader />;
   }
 
-  const currentRoom = rooms.find(room => room.id === chatId);
-
-  if (!currentRoom) {
-    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
-  }
   return (
     <>
       <div className="chat-top">
